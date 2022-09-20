78af8b7cbcbd46b7fa4094a3f6d5c7712005281d
JB <jacobbunnellca@gmail.com>
2022-09-20 11:40:22 -0400

    Function check even or odd then does multiplication on result

---
+++ JS exercises/Simple_multiplication.js
@@ -0,0 +1,9 @@
+function simpleMultiplication(number) {
+    // Check if even or odd then does simple multiplication
+    if (number % 2 == 0) {
+      return number * 8;
+    } else {
+      return number * 9;
+    }
+}
+

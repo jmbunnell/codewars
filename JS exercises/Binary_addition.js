ed8644167a48460786f585afbcbdc9dbd2df15b2
JB <jacobbunnellca@gmail.com>
2022-09-30 14:37:27 -0400

    Function to convert decimal number to binary

---
+++ JS exercises/Binary_addition.js
@@ -0,0 +1,7 @@
+// Adds two parameters together and returns binary conversion
+function addBinary(a,b){
+    // Summed the numbers & converted it to string with a 2 parameter so it would only be 0 & 1 (binary code)
+    return (a+b).toString(2)
+}
+
+

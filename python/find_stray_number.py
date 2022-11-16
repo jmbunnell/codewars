e00dc20047776c024fd9b2888d38a7a1957c7cb5
JB <jacobbunnellca@gmail.com>
2022-11-16 10:55:25 -0500

    Best practice added

--- python/find_stray_number.py
+++ python/find_stray_number.py
@@ -1,3 +1,9 @@
 # Finds stray/unique number in a list - ie. [1,1,2] returns 2
 def stray(arr):
     return [num for num in arr if arr.count(num) == 1][0]
+
+# Best Practice
+def stray(arr):
+    for x in arr:
+        if arr.count(x) == 1:
+            return x
